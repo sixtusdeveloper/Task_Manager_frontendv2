84019e9f388a67cfcf53dcddc3aa749cdfe5d179
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-23 00:29:36 +0100

    Resolved the conflict between the outdated HttpClientModule and provideHttpClient for the signup functionality and smooth performance

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,16 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app-routing.module';
 import { DemoAngularMaterialModule } from './app/DemoAngularMaterialModule';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideAnimations(),
+    provideHttpClient(),
     DemoAngularMaterialModule, // Add Material Module here
   ],
 }).catch((err) => console.error(err));
 
 
 
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { AppComponent } from './app/app.component';
-// import { provideRouter } from '@angular/router';
-// import { provideAnimations } from '@angular/platform-browser/animations';
-// import { routes } from './app/app-routing.module'; // Adjust based on your routes file
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     provideRouter(routes), // Provide your routes directly
-//     provideAnimations(),  // Add animations support if needed
-//   ],
-// }).catch((err) => console.error(err));
-
-
-
-
-
-// Original code
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
