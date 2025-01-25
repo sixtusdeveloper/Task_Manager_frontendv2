1e7b29df6c6eaa274750f276c9467d761e46ca64
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-25 23:56:00 +0100

    updated the poas.task.component

--- src/app/modules/admin/components/post-task/post-task.component.ts
+++ src/app/modules/admin/components/post-task/post-task.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-task',
@@ -8,13 +9,25 @@ import { AdminService } from '../../services/admin.service';
   styleUrl: './post-task.component.scss'
 })
 export class PostTaskComponent {
-  constructor(private adminService: AdminService ) {
+  taskForm!: FormGroup;
+  listOfEmployees: any = []; // Define the type of listOfEmployees
+  listOfPriorities: any = ["LOW", "MEDIUM", "HIGH"]; // Define the type of listOfPriorities
+
+  constructor(private adminService: AdminService, private fb: FormBuilder) {
     this.getUsers();
+    this.taskForm = this.fb.group({
+      employeeId: [null,[Validators.required]],
+      title: [null,[Validators.required]],
+      description: [null,[Validators.required]],
+      dueDate: [null,[Validators.required]],
+      priority: [null,[Validators.required]],
+    });
   }
 
   getUsers() {
     this.adminService.getUsers().subscribe(
       (response) => {
+        this.listOfEmployees = response;
         console.log(response);
       }
     );
