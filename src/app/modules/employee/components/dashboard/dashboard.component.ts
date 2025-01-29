f8505bbbd5e2c868e9fd7bcc9d34e41ad35477f2
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-29 23:07:13 +0100

    Implemented the get task by User ID in Angular

--- src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatPaginatorModule,
+    MatIconModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  ListOfTasks: any = [];
+  listOfTasks: any = [];
 
   constructor(private service: EmployeeService) {
     this.getTasks();
@@ -18,7 +30,7 @@ export class DashboardComponent {
   getTasks() {
     this.service.getEmployeeTasksById().subscribe((response) => {
       console.log(response);
-      this.ListOfTasks = response;
+      this.listOfTasks = response;
     });
   }
 }
